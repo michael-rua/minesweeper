5eeaadf61fe303d3c368303de46f875288712860
michael-rua <michaelrua1@gmail.com>
2020-07-23 14:33:12 +1200

    add board

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,46 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {
+  cells: [
+    {
+      row: 0, 
+      col: 0
+    },
+    {
+      row: 1, 
+      col: 1
+    }, 
+    {
+      row: 2,
+      col: 2
+      }, 
+    {
+      row: 3, 
+      col: 3
+    },
+    {
+      row: 4, 
+      col: 4
+    },
+    {
+      row: 5, 
+      col: 5
+    },
+    {
+      row: 6, 
+      col: 6
+    },
+    {
+      row: 7, 
+      col: 7
+    },
+    {
+      row: 8, 
+      col: 8
+    },
+  ]
+}
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
