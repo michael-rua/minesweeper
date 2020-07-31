79c8f08c3be8caa96bcc26b524e7fb700cec6367
michael-rua <michaelrua1@gmail.com>
2020-07-31 18:08:32 +1200

    add multiple board sizes

--- minesweeper.js
+++ minesweeper.js
@@ -1,138 +1,163 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = { // creating our board
-  cells: [
-    {
-      row: 0, 
-      col: 0, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 0, 
-      col: 1, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    }, 
-    {
-      row: 0,
-      col: 2, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    }, 
-    {
-      row: 0,
-      col: 3, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    }, 
-    {
-      row: 1, 
-      col: 0, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 1, 
-      col: 1, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 1, 
-      col: 2, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 1,
-      col: 3, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    }, 
-    {
-      row: 2, 
-      col: 0, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 2, 
-      col: 1, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 2,
-      col: 2, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 2,
-      col: 3, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 3,
-      col: 0, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 3,
-      col: 1, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 3,
-      col: 2, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 3,
-      col: 3, 
-      isMine: (Math.random() < 0.35),
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    }, 
-  ]
-}
+// var board = { // creating our board
+//   cells: [
+//     {
+//       row: 0, 
+//       col: 0, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 0, 
+//       col: 1, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     }, 
+//     {
+//       row: 0,
+//       col: 2, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     }, 
+//     {
+//       row: 0,
+//       col: 3, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     }, 
+//     {
+//       row: 1, 
+//       col: 0, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 1, 
+//       col: 1, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 1, 
+//       col: 2, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 1,
+//       col: 3, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     }, 
+//     {
+//       row: 2, 
+//       col: 0, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 2, 
+//       col: 1, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 2,
+//       col: 2, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 2,
+//       col: 3, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 3,
+//       col: 0, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 3,
+//       col: 1, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 3,
+//       col: 2, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 3,
+//       col: 3, 
+//       isMine: (Math.random() < 0.35),
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     }, 
+//   ]
+// }
+var board = {};
+var size = 4;
+function boardSize () {
+    //creates 'cells' empty array inside 'board' object
+    board.cells = []; 
+  
+    //loop within loop to set board size according to set value (default 4)
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        //add cell object with default values to each cells
+        let cell = {
+          row: i,
+          col: j,
+          isMine: (Math.random() < 0.35),
+          isMarked: false,
+          hidden: true,
+          surroundingMines: 0
+
+        }
+        //push cells into empty 'board'
+        board.cells.push(cell);
+        
+      }
+    }
+  }
 
 function startGame () {
   
@@ -192,10 +217,20 @@ function countSurroundingMines (cell) {
   
 }
 
-function reset() {
-window.location.reload(true);
-
+function reset(){  
+  document.querySelector(".board").innerHTML = " ";
+  size = parseInt(document.getElementById("boardSize").value);
+  
+  boardSize();
+  startGame();
 }
+boardSize()
 
+
+// function score() {
+//    var remain = document.getElementById("mines")
+//   let counter = board.cells.cell.isMine: True
+//   remain.innerText = "Total Mines: " + counter
+// }
 // create a board reset button. 
 // I want to reload the page as a system to reset the board
